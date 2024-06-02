1d627180eb0d9e1abc71d62f3c8f078b30f416e8
MRita011 <maria.rita04@edu.pucrs.br>
2024-06-01 23:25:56 -0300

    desenvolvimento topico 2

--- src/GestaoDeIngressos.java
+++ src/GestaoDeIngressos.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,4 +19,53 @@ public class GestaoDeIngressos {
         return ingressos;
     }
 
+    private LocalDate validarData(String data) {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            return LocalDate.parse(data, formatter);
+        } catch (DateTimeParseException e) {
+            System.out.println("Data inválida. Por favor, use o formato dd/MM/yyyy.");
+            return null;
+        }
+    }
+
+    public boolean emitirIngresso(String data, Visitante visitante) {
+        LocalDate dataIngresso = validarData(data);
+        if (dataIngresso == null) {
+            return false;
+        }
+        LocalDate dataAtual = LocalDate.now();
+        if (dataIngresso.isBefore(dataAtual)) {
+            System.out.println("Não é possível emitir um ingresso para uma data no passado.");
+            return false;
+        }
+        List<Ingresso> ingressosDoDia = ingressosPorData.getOrDefault(data, new ArrayList<>());
+
+
+        for (Ingresso ingresso : ingressosDoDia) {
+            if (ingresso.getVisitante().equals(visitante)) {
+                if (ingresso.isAtivo()) {
+                    System.out.println("O visitante já possui um ingresso ativo para esta data.");
+                    return false;
+                } else {
+
+                    ingresso.reativar();
+                    System.out.println("Ingresso reativado: " + ingresso.getIdIngresso());
+                    return true;
+                }
+            }
+        }
+
+        if (ingressosDoDia.size() < 500) {
+            Ingresso ingresso = new Ingresso(data, visitante, ingressosDoDia.size() + 1);
+            ingressos.add(ingresso);
+            ingressosDoDia.add(ingresso);
+            ingressosPorData.put(data, ingressosDoDia);
+            System.out.println("Ingresso emitido: " + ingresso.getIdIngresso());
+            return true;
+        } else {
+            System.out.println("Limite máximo de ingressos alcançado para o dia.");
+            return false;
+        }
+    }
 }
