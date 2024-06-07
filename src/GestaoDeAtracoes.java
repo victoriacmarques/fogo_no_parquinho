efd5325999516c333d200511f6cf86e87e9d894f
Pedro Tonial <ptpasinato@gmail.com>
2024-06-06 22:58:57 -0300

    Adicionando comentários na classe GestaoDeAtracoes

--- src/GestaoDeAtracoes.java
+++ src/GestaoDeAtracoes.java
@@ -1,15 +1,24 @@
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * Gerencia as atrações do parque.
+ */
 public class GestaoDeAtracoes {
     private List<Atracao> atracoes;
 
+    /**
+     * Inicializa a gestão de atrações com todas as atrações disponíveis.
+     */
     public GestaoDeAtracoes() {
         this.atracoes = new ArrayList<>();
         for (Atracao atracao : Atracao.values()) {
             this.atracoes.add(atracao);
         }
     }
+
+    /**
+     * Lista todas as atrações disponíveis no parque.
+     */
     public void listarAtracoes() {
         for (int i = 0; i < atracoes.size(); i++) {
             System.out.println((i + 1) + ". " + atracoes.get(i).getNome());
